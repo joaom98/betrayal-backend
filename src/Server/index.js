784f404efae2ddf62da336d1c0b56cb09510dd9e
joaom98 <joaomanoellima98@gmail.com>
2022-08-30 20:02:03 -0300

    Real fix

--- src/Server/index.js
+++ src/Server/index.js
@@ -1,7 +1,13 @@
 import http from 'http';
+import express from 'express';
+const app = express();
 const server = http.createServer(app);
 import { Server } from "socket.io";
 const io = new Server(server);
+
+app.get('/', (req, res, next) => {
+  res.send('lol')
+})
 
 io.on('connection', (socket) => {
   console.log('a user connected');
